3f843ec9ec7edd81cae61c30ea9411cab8875ef0
Daniel Quinn <dandidit.quinn@gmail.com>
2023-03-22 17:12:47 -0700

    .js additions

---
+++ script.js
@@ -0,0 +1,14 @@
+let dogs = prompt("Do you like Dogs? Yes or No? (Case Sensitive)");
+
+if (dogs == 'Yes'){
+    document.write("This is the page for you!!!");
+  } else if (dogs == 'No'){
+    document.write("Too bad here's Tux!");
+  }  else {
+    document.write("Tuxedos Page");
+  }
+
+  let usersName = prompt ("What is your name?");
+  document.write(" " + usersName + " is my favorite name!");
+
+  
